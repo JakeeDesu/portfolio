c2f086b5e43111a957c0453bb14f59eea030fb91
soguma <hamzamhindate@gmail.com>
2022-01-08 15:57:40 +0100

    add padding to 404 page

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ const NotFound = () => {
 	return (
 		<div className="flex flex-col justify-center items-center h-screen w-full bg-black text-gray-400 text-5xl">
 			<Eye darkTheme={true} />
-			<h1>THIS PAGE DOES NOT EXIST</h1>
+			<h1 className="py-10">THIS PAGE DOES NOT EXIST</h1>
 			<Link href="/" ><a className="m-10 text-gray-100 text-xl" >BACK HOME</a></Link>
 		</div>
 	);
